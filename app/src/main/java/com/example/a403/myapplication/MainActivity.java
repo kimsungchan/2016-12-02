e672abab85b2aed40428dc6dd58b07b84f1fb0ec
서영균 <vault961@gmail.com>
2016-12-02 18:55:57 +0900

    4, 날짜 설정을 선택하면 캘린더뷰가 보이고 기능 완성 5. 시간설정을 선택하면 타입피커가 보이고 기능 완성

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -28,18 +29,31 @@ public class MainActivity extends AppCompatActivity {
 
         b1 = (Button)findViewById(R.id.button);
         b2 = (Button)findViewById(R.id.button3);
-        rg = (RadioGroup)findViewById(R.id.radioGroup);
         r1 = (RadioButton)findViewById(R.id.radioButton);
         r2 = (RadioButton)findViewById(R.id.radioButton2);
-        cl = (CalendarView)findViewById(R.id.calendarView);
         t = (TextView)findViewById(R.id.textView);
         c = (Chronometer)findViewById(R.id.chronometer);
         tm = (TimePicker)findViewById(R.id.timePicker);
+        cl = (CalendarView)findViewById(R.id.calendarView);
+        rg = (RadioGroup)findViewById(R.id.radioGroup);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 c.start();
+            }
+        });
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radioButton){
+                    cl.setVisibility(View.VISIBLE);
+                    tm.setVisibility(View.INVISIBLE);
+                }
+                else if(checkedId == R.id.radioButton2){
+                    cl.setVisibility(View.INVISIBLE);
+                    tm.setVisibility(View.VISIBLE);
+                }
             }
         });
     }
